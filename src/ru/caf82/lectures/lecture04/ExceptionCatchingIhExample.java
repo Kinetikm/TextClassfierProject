a569fe5b44babd85d32914edac74d8710b2e5291
ilysko <ilya.lysko@gmail.com>
2017-09-24 13:04:04 +0300

    Examples for lecture04 from Ilya.

--- src/ru/caf82/lectures/lecture04/ExceptionCatchingIhExample.java
+++ src/ru/caf82/lectures/lecture04/ExceptionCatchingIhExample.java
@@ -3,14 +3,14 @@ package ru.caf82.lectures.lecture04;
 public class ExceptionCatchingIhExample {
 
     public static void main(String[] args) {
-        try {
-            System.out.println(6 / 0);
-        } catch (Exception ex) {
-            System.out.println("Ошибка верхнего увроня");
-        } catch (ArithmeticException ex) {
-            System.out.println("Ошибка! Ошибка!");
-            System.out.println(ex.toString());
-        }
+//        try {
+//            System.out.println(6 / 0);
+//        } catch (Exception ex) {
+//            System.out.println("Ошибка верхнего увроня");
+//        } catch (ArithmeticException ex) {
+//            System.out.println("Ошибка! Ошибка!");
+//            System.out.println(ex.toString());
+//        }
 
         try {
             System.out.println(Long.parseLong("Mama"));
