0db07f5cce0220cf3b395da825fffc7bfaf04b54
vyshka97 <maks081197@yandex.ru>
2017-09-28 00:19:54 +0300

    new commit

---
+++ result/src/main/java/ru/caf82/result/machinelearning/preprocessing/CountVectorizer.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.java.ru.caf82.result.machinelearning.preprocessing;
+
+/**
+ *
+ * @author 1
+ */
+public class CountVectorizer implements SequenceProcessor, Transformer{
+    
+}
