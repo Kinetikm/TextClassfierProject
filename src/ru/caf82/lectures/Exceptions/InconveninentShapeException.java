b22b241f635b2eb9c0f31c9a0dd2d24d866b2e5c
SychDan <sych.daniil@yandex.ru>
2017-09-28 16:48:32 +0300

    3

---
+++ src/ru/caf82/lectures/Exceptions/InconveninentShapeException.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Exceptions;
+/**
+ *
+ * @author User
+ */
+public class InconveninentShapeException {
+    public InconveninentShapeException(){
+        
+    }
+    public InconveninentShapeException(String message){
+        
+    }
+    public InconveninentShapeException(String message, Throwable cause){
+        
+    }
+    public InconveninentShapeException(Throwable cause){
+        
+    }
+}
--- src/ru/caf82/lectures/Other/MathService.java
+++ src/ru/caf82/lectures/Other/MathService.java
@@ -5,6 +5,7 @@
  */
 import java.util.Arrays;
 import java.util.Random;
+import Exceptions.InconveninentShapeException;
 
 /**
  *
