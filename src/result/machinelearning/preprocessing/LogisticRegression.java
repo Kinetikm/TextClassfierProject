7a148ac82ee772507b13bd93ef6d514681690dd4
SychDan <sych.daniil@yandex.ru>
2017-10-08 16:57:05 +0300

    Update

--- src/result/machinelearning/preprocessing/LogisticRegression.java
+++ src/result/machinelearning/preprocessing/LogisticRegression.java
@@ -3,145 +3,93 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package result.machineLearning.preprocessing;
+package result.machinelearning.preprocessing;
+
+import Exeptions.EmptyArrayException;
+import Exeptions.InconveninentShapeException;
+import java.io.Serializable;
+import java.util.Random;
+import Other.MathService;
+
 /**
  *
  * @author User
  */
-import Exeptions.InconveninentShapeException;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Serializable;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
-import result.machinelearning.preprocessing.MlModel;
-public class LogisticRegression implements MlModel, Serializable{
+public class LogisticRegression implements MlModel, Serializable {
 private float alpha;
 private float betta;
-private int maxIter;
 private boolean parralize;
 private float learnRate;
 private Random initializer = new Random();
+private int ITERATES=1000;
+private double rate=0.001;
+
 private double[] weights;
-private int ITERATIONS=1000;
 private boolean fitted = false;
-public static class Instance {
-public int label;
-public int[] x;
 
-public Instance(int label, int[] x) {
-this.label = label;
-this.x = x;
-}
-}
-private double classify(int[] x) {
-double logit = .0;
-for (int i=0; i<weights.length;i++) {
-logit += weights[i] * x[i];
-}
-return sigmoid(logit);
-}
-private static double sigmoid(double z){
-    return 1.0/(1.0+Math.exp(-z));
-}
-
-
- public LogisticRegression(float alpha, float betta, int maxIter, float learnRate, boolean parralize) {}
-   
- 
-       public void train(List<Instance> instances) {
- for (int n=0; n<ITERATIONS; n++) {
- double lik = 0.0;
- for (int i=0; i<instances.size(); i++) {
- int[] x = instances.get(i).x;
- double predicted = classify(x);
- int label = instances.get(i).label;
- for (int j=0; j<weights.length; j++) {
- weights[j] = weights[j] + rate * (label - predicted) * x[j];
- }
- // not necessary for learning
- lik += label * Math.log(classify(x)) + (1-label) * Math.log(1- classify(x));
- }
- System.out.println("iteration: " + n + " " + Arrays.toString(weights) + " mle: " + lik);
- }
- }
-
-       /*double []sm1=null;
-       double []sm2=null;
-       double []z=null;
-       for(int i=0;i<X.length;i++){
-           weights=new double[X.length];
-           z=new double[X.length];
-           for(int j=0;j<X.length;j++)z[i]=z[i]+weights[j]*X[i][j];
-               }
-       for(int i=0;i<X.length;i++){
-           weights=new double[X.length];
-           sm1=new double[X.length];
-           sm2=new double[X.length];
-           for(int j=0;j<X.length;j++){
-           sm1[i]=sm1[i]*Math.log(1/(1+Math.exp(-z[i])));
-           sm1[i]=sm1[i]*Math.log(1/(1+Math.exp(-z[i])));
+  // public LogisticRegression2(float alpha, float betta, int maxIter, float learnRate, boolean parralize) {}
+   public static double sigmoid(double z){
+       return 1/(1+Math.exp(-z));
+   }   
+   static public double[][] init(double[][]X){
+       int i,j;
+       double [][]Y=new double[X.length][X.length+1];
+       for(i=0;i<X.length;i++){
+           Y[i][1]=1;
+           for(j=1;j<X[i].length+1;j++)Y[i][j]=X[i][j];
        }
-   }*/
+       return Y;
+   };
+   public void train(double[][] X, int[] y) throws InconveninentShapeException, EmptyArrayException {
+       double zz=0;
+       double pr=0;
+       double []sumdelta;
+       int i,j;
+       weights=new double[X.length+1];
+       X=init(X);
+       double delta[]=null;
+       sumdelta=new double[X.length];
+       for(int u=0;u<ITERATES;u++){
+           delta=new double[X.length];
+           for(i=0;i<X.length;i++){
+               zz=MathService.doProduct(X[i], weights);
+               pr=sigmoid(zz);
+               for(j=0;j<X[i].length;j++)delta[i]=(pr-y[i])*X[i][j];
+               for(i=0;i<X.length;i++) sumdelta[i]=sumdelta[i]+delta[i];
+       }
+           for(i=0;i<X.length;i++) sumdelta[i]=sumdelta[i]/X.length;
+           for(i=0;i<X.length;i++)weights[i]=weights[i]-rate*sumdelta[i];
+       }
    }
-   public int predict(double[] X) throws ModelNotFittedException, InconveninentShapeException 
-   {
+   public double ss(double[] X){
+      double result=0;
+       for(int i=0;i<X.length;i++)result=result+X[i]*weights[i];
+       return result;
    }
-   public double[] predictProba(double[] X)// throws ModelNotFittedException, InconveninentShapeException 
-    {
-       
-    }
-   public void saveToFile(String filename) throws IOException {
-       File Input = new File(filename);
-            try{
-                if(!Input.exists())Input.createNewFile();
-            }
-            catch(IOException e){
-                throw new RuntimeException(e);
-            }
-            System.out.println("Write the text");
-            double s=0;
-            for(double x: weights)s=x;
-            FileWriter fr = null;
-            BufferedWriter br = null;
-            try {
-                fr = new FileWriter(Input,true);
-                br = new BufferedWriter(fr);
-                br.newLine();
-                br.append((char) s);                        
-            } 
-            catch (IOException e) {
-                e.printStackTrace();
-            } 
-            finally{
-                try {
-                    br.close();
-                    fr.close();
-                } 
-                catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+   public double predict(double[] X) throws InconveninentShapeException {
+       double ver=0;
+       double v=0;
+       ver =Math.exp(ss(X))/(1+Math.exp(ss(X)));
+       if(ver>0.5)System.out.println("It is a dog");else if(ver<0.4)System.out.println("It is a cat");
+       else System.out.println("I don't know");
+       return ver;
    }
-   public double[] getWeights() {
-       return;
-   }
-   public float getAlpha() {}
-   public void setAlpha(float alpha) {}
-   public float getBetta() {}
-   public void setBetta(float betta) {}
-   public int getMaxIter() {}
-   public void setMaxIter(int maxIter) {}
-   public boolean isParralize() {}
-   public void setParralize(boolean parralize) {}
-   public float getLearnRate() {}
-   public void setLearnRate(float learnRate) {}
-   private double lossFunction(double[][] X, double[] W, int[] y) throws InconveninentShapeException {}
-   private double[] lossFunctionDerivative(double[][] X, double[] W,
-                                           int[] y, float alpha, float betta) throws InconveninentShapeException {}
-
-
+   
 }
+//   public double[] predictProba(double[] X) throws ModelNotFittedException, InconveninentShapeException {}
+//   public void saveToFile(String filename) throws IOException {}
+//   public double[] getWeights() {}
+//   public float getAlpha() {}
+//   public void setAlpha(float alpha) {}
+//   public float getBetta() {}
+//   public void setBetta(float betta) {}
+//   public int getMaxIter() {}
+//   public void setMaxIter(int maxIter) {}
+//   public boolean isParralize() {}
+//   public void setParralize(boolean parralize) {}
+//   public float getLearnRate() {}
+//   public void setLearnRate(float learnRate) {}
+//   private double lossFunction(double[][] X, double[] W, int[] y) throws InconveninentShapeException {}
+//   private double[] lossFunctionDerivative(double[][] X, double[] W,
+//                                           int[] y, float alpha, float betta) throws InconveninentShapeException {}
+//}
